32a9dd3c3be87c858ee5bebd2e1007e41b84115f
xiechao06 <xiechao06@gmail.com>
2019-04-11 17:35:11 +0800

    Updates

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"createSchemas","url":"globals.html#createschemas","classes":"tsd-kind-function"},{"id":1,"kind":64,"name":"clearSchemas","url":"globals.html#clearschemas","classes":"tsd-kind-function"}]};
